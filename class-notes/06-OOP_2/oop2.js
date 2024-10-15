cbe204e1c76ab3fb62fcb38e23187c8233a6c61e
Ayca96 <aycakubra1@gmail.com>
2024-10-15 10:28:44 +0200

    "Refactored OOP example code: added Vehicle class, inheritance, and new Car and Track classes; updated constructor and method implementations"

--- class-notes/06-OOP_2/oop2.js
+++ class-notes/06-OOP_2/oop2.js
@@ -34,12 +34,54 @@ console.log(NewInstance);
 console.log(NewInstance.methodName ());
 
 
-//* constructor
+//* constructor yapici
 
-class Car{
+// class Car{
+//   isRunning=false
+//   //brand='noname'
+//   constructor ( brand,model,year) {
+//     this.brand=brand,
+//     this.model=model,
+//     this.year=year
+//   }
+//   runEngine(){
+//     this.isRunning=true
+//     return this.isRunning
+
+//   }
+// }
+
+// const Opel=new Car('Opel','Corsa','2020')
+// console.log(Opel); 
+
+// Ayni isimde birden fazla method olusturulabilir fark aldigi parametreler. AMA JS BUNU DESTEKLEMIYOR.
+
+/*console.log(Opel.runEngine());
+
+const Mercedes = new Car ('Mercedes', 'E200', 2023)
+
+console.log((Mercedes));
+console.log(Mercedes.runEngine()); */
+
+//? Inheritance = Miras Alma 'vererben'
+
+// Baska bir class in sahip oldugu her seyi aliyor + kendi sahip olduklari
+
+class Vehicle{
+  isActive=false
+  seatCount=5
+  hp
+  constructor (vehicleType){
+    this.vehicleType=vehicleType
+  }
+}
+
+class Car extends Vehicle{
   isRunning=false
+  
   //brand='noname'
-  constructor ( brand,model,year) {
+  constructor ( brand,model,year=1900,vehicleType) {
+    super(vehicleType)
     this.brand=brand,
     this.model=model,
     this.year=year
@@ -51,10 +93,37 @@ class Car{
   }
 }
 
-const Opel=new Car('Opel','Corsa','2020')
-console.log(Opel);
+const Mercedes = new Car ('Mercedes', 'E200', 2023, 'Car')
 
-// Ayni isimde birden fazla method olusturulabilir fark aldigi parametreler. AMA JS BUNU DESTEKLEMIYOR.
+//console.log((Mercedes));
+// console.log(Mercedes.runEngine()); 
 
-console.log(Opel.runEngine());
+class Track extends Vehicle{
+  isRunning=false
+  maxCapacity
+  //brand='noname'
+  constructor ( brand='noname',model,year=1900,vehicleType) {
+    super(vehicleType)
+    this.brand=brand,
+    this.model=model,
+    this.year=year
+  }
+  runEngine(){
+    this.isRunning=true
+    return this.isRunning
+
+  }
+}
+
+class Accessory extends Car{
+constructor(accessoryName,brand,model,year,vehicleType){
+  super(brand,model,year,vehicleType)
+  this.accessoryName=accessoryName
+}
+}
+
+const Seat= new Accessory('Leader', 'Audi', 'Q8', 2022, 'Car')
+console.log(Seat);
+
+// POLYMORPHISM
 
