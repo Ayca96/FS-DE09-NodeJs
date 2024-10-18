1d4a39a9bdb6e1effd99d2608d6ee5653842a60a
Ayca96 <aycakubra1@gmail.com>
2024-10-18 12:23:32 +0200

    "Removed comments and whitespace, added new HTTP server code with routing and response handling"

--- class-notes/08-NodejsServer/index.js
+++ class-notes/08-NodejsServer/index.js
@@ -4,9 +4,7 @@
 
 //           NodeJs Server
 
-// /* --------------------------*/   
-
- 
+// /* --------------------------*/
 
 // // HTTP SERVER:
 // const http =require('node:http') // built in module
@@ -14,8 +12,7 @@
 // const PORT = process.env.PORT
 // const HOST = process.env.HOSTNAME
 
-
-// // require('node:http'), Node.js içinde HTTP protokolünü kullanarak sunucu oluşturmak ve HTTP isteklerini işlemek için gerekli olan http modülünü içe aktarmak için kullanılan bir ifadedir. 
+// // require('node:http'), Node.js içinde HTTP protokolünü kullanarak sunucu oluşturmak ve HTTP isteklerini işlemek için gerekli olan http modülünü içe aktarmak için kullanılan bir ifadedir.
 
 // // http.createServer((request,response)=>{...})
 
@@ -23,13 +20,12 @@
 
 //   response.end('Hallo world response end')
 //   console.log('Hello world from console');
-  
+
 // })
 
 // // DEFAULT SERVER DOMAIN (local domain) => localhost : 127.0.0.1
 // app.listen(PORT,()=> console.log('Server running')
 // )
-
 
 // require('dotenv').config() // en yukari yazilmak zorunda.
 
@@ -37,11 +33,7 @@
 // // process.env ile bu değişkenlere uygulamanın herhangi bir yerinde erişilebilir.
 // // Bu yöntem, uygulamanın güvenliğini artırır ve hassas bilgileri kod içinde doğrudan yazmak zorunda kalmazsınız.
 
-
 // console.log('Hello world', PORT , HOST);
-
-
-
 
 "use strict";
 /* -------------------------------------------- *
@@ -55,6 +47,7 @@ const HOST = process.env.HOST;
 
 // HTTP SERVER:
 const http = require("node:http"); // built in module
+const { stringify } = require("node:querystring");
 
 /* -------------------------------------------- */
 
@@ -72,17 +65,63 @@ const http = require("node:http"); // built in module
 
 /* -------------------------------------------- */
 
+// http.createServer((req, res) => {
+//   // console.log(req);
+//  // console.log(res);
+
+//   console.log(req.url);
+//   if (req.url == '/') {
+//       res.end('<h1>this is Home Page</h1>')
+//   } else if (req.url == '/about') {
+//       res.end('<h1>this is about Page</h1>')
+//   } else {
+//       res.end('<h1>this is any Page</h1>')
+//   }
+// }).listen(PORT,HOST () => console.log(`Server is running at : http://${HOST}:${PORT}`))
+
+// const newApp= http.createServer((req,res)=>{
+
+// if(req.url=='/'){
+//   res.end('<h2>Hier ist Startseite</h2>')
+// }else if (req.url == '/uns'){
+//   res.end('<h2>Hier ist über uns</h2>')
+// }else {
+//   res.end('<h2>Hier ist andere Seite</h2>')
+// }
+
+// })
+
+// newApp.listen(PORT, ()=>console.log(`Server läuft auf : http://${HOST}:${PORT}`))
 
 http.createServer((req, res) => {
-  // console.log(req);
- // console.log(res);
-  
-  console.log(req.url);
-  if (req.url == '/') {
-      res.end('<h1>this is Home Page</h1>')
-  } else if (req.url == '/about') {
-      res.end('<h1>this is about Page</h1>')
+
+  /* res.write('<h1>this is write - 1 </h1>')
+     res.write('<h1>this is write - 2 </h1>')
+     res.write('<h1>this is write - 3 </h1>')
+     res.write('<h1>this is write - 4 </h1>') */
+
+  if (req.method == 'GET') {
+
+      // setHeader (single header method)
+      res.setHeader('title', 'value')
+
+      // writeHead ( statusCode, statusMessage, {multi headers})
+      res.writeHead(200, {
+          'content-encoding': 'utf-8',
+          'multi-headers-exp': "test header value"
+      })
+
+      const obj = {
+          result: true,
+          message: 'Hello world!'
+      }
+
+      res.write(JSON.stringify(obj))
+      res.end()
+
   } else {
-      res.end('<h1>this is any Page</h1>')
+    res.writeHead(400)
+      res.end('Wrong Method!')
   }
+
 }).listen(PORT, () => console.log(`Server is running at : http://${HOST}:${PORT}`))
