68b7ad14c9bda975b8cd71354fb440df7f33c55d
Ayca96 <aycakubra1@gmail.com>
2024-10-21 11:05:38 +0200

    "Updated app.js to modify root route response and add various route examples with different HTTP methods and regex patterns"

--- class-notes/09-Express/app.js
+++ class-notes/09-Express/app.js
@@ -34,10 +34,41 @@ const HOST = process.env.HOST || '127.0.0.1'
 
 
 app.get('/',(req,res)=>{
-  res.write('clarusway')
+ /* res.write('clarusway')
   res.write('hallo')
-  res.end() // bitiriyor. burdan sonra write yazamayiz artik.
+  res.end()*/ // bitiriyor. burdan sonra write yazamayiz artik.
+
+//   res.write(JSON.stringify( {  // objeyi yazamaz
+//     message:'clarusway'
+// }))
+// res.end()
+
+  res.send({ // obje dönüsümü yapar.
+    message:'clarusway'
+  })
+
 })
+
+// app.get('/',(req,res)=>{res.send('method GET')})
+// app.post('/',(req,res)=>{res.send('method POST')})
+// app.put('/',(req,res)=>{res.send('method PUT')}) // PUT = PATCH
+// app.patch('/',(req,res)=>{res.send('method PATCH')}) // PUT = PATCH
+// app.delete('/',(req,res)=>{res.send('method DELETE')})
+
+//app.all('/',(req,res)=>{res,send('method GET')})  // tavsiye edilmez.
+
+//app.get('/abc(x?)123',(req,res)=>{res.send('method GET')}) //  ? varsa ya da yoksa calistirir.
+//app.get('/abc(x+)123',(req,res)=>{res.send('method GET')})// + yazinca  n adet x yazilabilir
+//app.get('/abc(x*)123',(req,res)=>{res.send('method GET')})// * arada ne olursa olsun veriyi ceker.
+
+//app.get(/abc/,(req,res)=>{res.send('method GET')})
+ // tek tirnaklar yok burda. Bu bir regex ifadesidir. Bu ifade, URL'de "abc" dizisinin herhangi bir yerde geçtiği yollarla eşleşir.
+app.get(/abc$/,(req,res)=>{res.send('method GET')}) //Bu rota, URL'nin tamamen /abc ile bitmesini bekler.
+
+app.get(/^\/abc/,(req,res)=>{ res.send( ' method GET') }) //  sonuna ne gelirse gelsin
+
+
+//$: Düzenli ifadelerde, $ işareti "sonu" temsil eder. Yani bu rota, URL'nin sonunda "abc" ile bitmesini zorunlu kılar.
 
 
 
@@ -46,3 +77,5 @@ app.listen(8000, ()=> console.log(`Server running on http://${HOST}:${PORT}`)
 
 
  // nodemon app.js yazdik cünkü package.json da main olarak app yazili. app.js yazmadan da calisirdi default alirdi ama baska dosya olursa ismini belirtmemiz lazim.
+
+ //https://regexr.com
