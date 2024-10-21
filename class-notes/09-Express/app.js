c95a6c3f172a1e1fe3343b6805b97f57a868606d
Ayca96 <aycakubra1@gmail.com>
2024-10-21 10:30:16 +0200

    "Modified app.js: commented out original response, added new response with 'hallo', and attempted to write an object as response"

--- class-notes/09-Express/app.js
+++ class-notes/09-Express/app.js
@@ -17,9 +17,32 @@ const express = require ('express')
 
 const app= express() // express üzerinde server calistirir
 
+// dotenv 
+/*
+const x=require('dotenv')
+x.config() // config dosyasini okur
+*/
+
+require('dotenv').config() // dotenv deki tüm degiskenler process env ye alinir.
+//console.log(process.env);
+
+
+const PORT = process.env.PORT || 8000
+const HOST = process.env.HOST || '127.0.0.1'
+
 // app.listen (PORT=8000)  Ein Port erlaubt die Kommunikation zwischen Ihrem Computer mit anderen Computern sowie mit dem Internet. 
 
-app.listen(8000, ()=> console.log('Server running')
+
+app.get('/',(req,res)=>{
+  res.write('clarusway')
+  res.write('hallo')
+  res.end() // bitiriyor. burdan sonra write yazamayiz artik.
+})
+
+
+
+app.listen(8000, ()=> console.log(`Server running on http://${HOST}:${PORT}`)
 )
 
-// nodemon app.js yazdik cünkü package.json da main olarak app yazili. app.js yazmadan da calisirdi default alirdi ama baska dosya olursa ismini belirtmemiz lazim.
+
+ // nodemon app.js yazdik cünkü package.json da main olarak app yazili. app.js yazmadan da calisirdi default alirdi ama baska dosya olursa ismini belirtmemiz lazim.
