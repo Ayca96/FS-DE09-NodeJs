b55bdb47126b9ad59ef81d2d0995c2ab457cdb3a
Ayca96 <aycakubra1@gmail.com>
2024-10-21 09:40:26 +0200

    "Added Express.js server setup and dependencies to package.json"

--- class-notes/09-Express/app.js
+++ class-notes/09-Express/app.js
@@ -4,3 +4,22 @@
 Express.js
 */
 
+/*
+ npm init -y
+ npm i express dotenv
+
+*/
+
+
+//? Express server start
+
+const express = require ('express')
+
+const app= express() // express üzerinde server calistirir
+
+// app.listen (PORT=8000)  Ein Port erlaubt die Kommunikation zwischen Ihrem Computer mit anderen Computern sowie mit dem Internet. 
+
+app.listen(8000, ()=> console.log('Server running')
+)
+
+// nodemon app.js yazdik cünkü package.json da main olarak app yazili. app.js yazmadan da calisirdi default alirdi ama baska dosya olursa ismini belirtmemiz lazim.
