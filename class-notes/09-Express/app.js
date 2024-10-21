5f4804cd1640984d154bc016c89a007fec1e9fd0
Ayca96 <aycakubra1@gmail.com>
2024-10-21 12:09:13 +0200

    "Removed and commented out code blocks in app.js, added new code for URL parameters and status codes, and modified the app.listen call."

--- class-notes/09-Express/app.js
+++ class-notes/09-Express/app.js
@@ -30,24 +30,22 @@ require('dotenv').config() // dotenv deki tüm degiskenler process env ye alinir
 const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || '127.0.0.1'
 
-// app.listen (PORT=8000)  Ein Port erlaubt die Kommunikation zwischen Ihrem Computer mit anderen Computern sowie mit dem Internet. 
 
+// app.get('/',(req,res)=>{
+//  /* res.write('clarusway')
+//   res.write('hallo')
+//   res.end()*/ // bitiriyor. burdan sonra write yazamayiz artik.
 
-app.get('/',(req,res)=>{
- /* res.write('clarusway')
-  res.write('hallo')
-  res.end()*/ // bitiriyor. burdan sonra write yazamayiz artik.
+// //   res.write(JSON.stringify( {  // objeyi yazamaz
+// //     message:'clarusway'
+// // }))
+// // res.end()
 
-//   res.write(JSON.stringify( {  // objeyi yazamaz
+//   res.send({ // obje dönüsümü yapar.
 //     message:'clarusway'
-// }))
-// res.end()
+//   })
 
-  res.send({ // obje dönüsümü yapar.
-    message:'clarusway'
-  })
-
-})
+// })
 
 // app.get('/',(req,res)=>{res.send('method GET')})
 // app.post('/',(req,res)=>{res.send('method POST')})
@@ -63,15 +61,60 @@ app.get('/',(req,res)=>{
 
 //app.get(/abc/,(req,res)=>{res.send('method GET')})
  // tek tirnaklar yok burda. Bu bir regex ifadesidir. Bu ifade, URL'de "abc" dizisinin herhangi bir yerde geçtiği yollarla eşleşir.
-app.get(/abc$/,(req,res)=>{res.send('method GET')}) //Bu rota, URL'nin tamamen /abc ile bitmesini bekler.
+//app.get(/abc$/,(req,res)=>{res.send('method GET')}) //Bu rota, URL'nin tamamen /abc ile bitmesini bekler.
 
-app.get(/^\/abc/,(req,res)=>{ res.send( ' method GET') }) //  sonuna ne gelirse gelsin
+//app.get(/^\/abc/,(req,res)=>{ res.send( ' method GET') }) //  sonuna ne gelirse gelsin
 
 
 //$: Düzenli ifadelerde, $ işareti "sonu" temsil eder. Yani bu rota, URL'nin sonunda "abc" ile bitmesini zorunlu kılar.
 
 
 
+
+//? url Parametre
+
+// app.get('/user/:userId/:name',(req,res)=>{
+//   res.send({
+//      url: {
+//          protocol:req.protocol,
+//          secure:req.secure,
+//          hostname:req.hostname,
+//          subdomain:req.subdomains,
+//          url:req.url,
+//          originalURL:req.originalUrl, // hangi routerdaysak onu verir isleyecegiz bu konuyu
+//          params:req.params,
+//          method:req.method,
+//          query:req.query
+//      },
+//      id:req.params.userId
+//  })
+// })
+
+//? Status Codes
+//todo default status code 200
+// app.get('/', (req, res) => {res.status(200).send({ message: 'ok' })});
+// app.put('/', (req, res) => {res.status(201).send({ message: 'ok' })});
+// app.post('/', (req, res) => {res.status(202).send({ message: 'ok' })});
+// app.get('/', (req, res) => {res.status(203).send({ message: 'ok' })});
+
+app.get('/download',(req,res)=>{res.download ('./app.js','newName.js')}) // newName app.js nin yeni adi degistirmis oluyoruz.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.listen (PORT=8000)  Ein Port erlaubt die Kommunikation zwischen Ihrem Computer mit anderen Computern sowie mit dem Internet. 
 app.listen(8000, ()=> console.log(`Server running on http://${HOST}:${PORT}`)
 )
 
