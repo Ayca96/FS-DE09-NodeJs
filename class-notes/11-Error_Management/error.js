4de2514d1e372a4cac3d030063829ae378ba1d19
Ayca96 <aycakubra1@gmail.com>
2024-10-24 10:21:05 +0200

    "Added error handling to Express.js app: implemented route with error throwing, created error handler function, and registered it with app.use"

--- class-notes/11-Error_Management/error.js
+++ class-notes/11-Error_Management/error.js
@@ -19,4 +19,54 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, ()=>console.log(`Running at : http://127.0.0.1:${PORT}` ))
+/*----------------------------------------*/
+
+app.get('/user/:id', (req,res)=>{
+const {id} =req.params
+
+
+if(isNaN(id)){  // isNaN= is not a Number demek ve böyleyse true döner.
+ // res.errorStatusCode = 400
+  throw new Error ('id is not a number',{cause:'hata params tan gelen id den kaynakli'})
+  
+} else{
+  res.send({
+  error:false,
+  message:'hi there',
+  id // id:id yazmak yerine sadece id yazsak yeterli key value isimleri ayni oldugu zamanlarda
+})
+}
+
+
+})
+
+
+/*---------------------------------------------*/
+
+const errorHandler = (error,req,res,next)=>{
+//console.log(error);
+const statusCode= res?.errorStatusCode ?? 500
+
+res.status(statusCode).send({
+  error:true,
+  message: error.message, //' error handler runnig'
+  cause:error.cause,
+ 
+})
+
+}
+
+app.use(errorHandler)
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>console.log(`Running at : http://127.0.0.1:${PORT}` )) // hep asagida durmasi lazim.
