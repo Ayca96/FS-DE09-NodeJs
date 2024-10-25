ae6a533b1ee4623a5b4ab87bfad8180b851f4f9a
Ayca96 <aycakubra1@gmail.com>
2024-10-25 10:46:55 +0200

    "Added Sequelize configuration and model definition for 'todos' table"

--- class-notes/12-ToDo_Sequelize_1/index.js
+++ class-notes/12-ToDo_Sequelize_1/index.js
@@ -17,6 +17,65 @@ app.all('/', (req, res) => {
     res.send('WELCOME TO TODO API')
 })
 
+/**********************************************************/
+// Sequelize
+
+const {Sequelize,DataTypes}=require('sequelize')
+
+// Creating new instance
+const sequelize = new Sequelize('sqlite:./db.sqlite3') 
+
+// Creating Model
+//sequelize.define('todos', {attributes})
+
+sequelize.define('todos',{
+ /* id:{ // this attribute created auto
+    type:DataTypes.INTEGER,
+    allowNull:false, // default true
+    unique:true, // default false
+    comment:'this is comment',
+    primaryKey:true, // default false
+    autoIncrement: true, //default false
+    field:'custom name',
+    defaultValue:0 //default : null
+  } */
+
+ title:{
+  type:DataTypes.STRING,
+  allowNull:false
+ },
+ description:DataTypes.TEXT,  // =>shorthand using, uzun hali type:DataTypes.TEXT
+
+ priority:{ // -1:low, 0:normal, 1:high
+  type:DataTypes.TINYINT,
+  allowNull:false, // Bu alan NULL değer kabul etmez
+  defaultValue:0
+ },
+
+ isDone:{
+  type:DataTypes.BOOLEAN,
+  allowNull: false,
+  defaultValue: false,
+ }
+// No need to define created At and updatedAt Created auto:
+})
+
+// Sync
+
+sequelize.sync()
+
+
+
+
+//Connecting to DB
+sequelize.authenticate()
+.then(()=>console.log('*DB connected*'))
+.catch(()=>console.log('*DB not connected*'))
+
+
+
+
+
 // continue from here...
 
 const errorHandler = (err, req, res, next) => {
