e122a2bc0cf87dfa50b2037cbb9ecaa999673b02
Ayca96 <aycakubra1@gmail.com>
2024-10-28 10:24:41 +0100

    "Added environment variables for database path and name, updated index.js to use these variables, and implemented CRUD operations for Todo model"

--- class-notes/12-ToDo_Sequelize_1-2/index.js
+++ class-notes/12-ToDo_Sequelize_1-2/index.js
@@ -8,7 +8,8 @@ const app = express();
 require('express-async-errors')
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-
+const DB_PATH = process.env.DB_PATH || './db.sqlite3';
+const DB_NAME = process.env.DB_NAME || './db.sqlite3';
 /* ------------------------------------------------------- */
 // Accept json data:
 app.use(express.json())
@@ -23,7 +24,7 @@ app.all('/', (req, res) => {
 const {Sequelize,DataTypes}=require('sequelize')
 
 // Creating new instance
-const sequelize = new Sequelize('sqlite:./db.sqlite3') // define your db and the path
+const sequelize = new Sequelize(`${DB_NAME}:${DB_PATH}`) // define your db and the path
 
 // Creating Model
 //sequelize.define('modelName', {attributes})
@@ -77,21 +78,22 @@ sequelize.authenticate() // orm de calisirken bütün fonksiyonlar asenkrondur.
 //Routes
 const router= express.Router()
 
-//Read Todo
+//List Todo
 
 router.get('/todo', async(req,res)=>{
 
- const result= await Todo.findAll() // findAll bir nevi select yani okuma anlaminda read gibi.
+ //const result= await Todo.findAll() // findAll bir nevi select yani okuma anlaminda read gibi.
 
- const results= await Todo.findAndCountAll() // count getiriyor ekstra
+ const result= await Todo.findAndCountAll() // count getiriyor ekstra
 
  res.status(200).send({
     error:false,
     result
-    //results
+    
   })
 })
 
+// CRUD OPERATIONS
 
 // Create Todo
 router.post('/todo',async (req,res)=>{
@@ -114,35 +116,61 @@ router.post('/todo',async (req,res)=>{
  
 })
 
+
+
+// Read Todo
+
+router.get('/todo/:id', async(req,res)=>{
+  
+  // const result = await Todo.findOne({where:{id:req.params.id}})
+
+  const result = await Todo.findByPk(req.params.id)
+
+  res.status(200).send({
+    error:false,
+    result
+  })
+})
+
+
+// Update Todo
+
+router.put('/todo/:id', async(req,res)=>{
+
+//const result = await Todo.update({...newData},{...filter})
+const result = await Todo.update(req.body,{where: {id:req.params.id}})
+const isUpdated = result[0]
+res.status(isUpdated ? 202 : 404).send({
+  error:isUpdated ? false : true,
+  message:isUpdated ? 'Updated':"Something went wrong",
+  updatedData : isUpdated && await Todo.findByPk(req.params.id)
+})
+
+})
+
+
+
+
 // Delete Todo
 router.delete('/todo/:id', async (req, res) => {
   const { id } = req.params;
-  const deleted = await Todo.destroy({
+  const isDeleted = await Todo.destroy({
       where: { id }
   });
 
-  if (deleted) {
-      res.status(204).send();
-  } else {
-      res.status(404).send({
-          error: true,
-          message: 'Todo not found',
-      });
-    }
+ if(isDeleted){
+  res.sendStatus(204)
+ }else{
+  res.errorStatusCode = 404
+  throw new Error('Can not deleted ! or Maybe already deleted.')
+ }
+
+  res.status(202).send({
+    error: false,
+  
+  })
+
 });
-
-// router.delete('/todo/:id',async (req,res)=>{
-//   const results =await Todo.destroy({
-//     where:{id:req.params.id}
-//   })
-//   res.status(200).send({
-//     error:false,
-//     results
-//   })
-// })
-
-
-
 
 
 
