d195dde39fbf0a8685ce3d0c5c539f7c46d756da
Ayca96 <aycakubra1@gmail.com>
2024-11-01 09:50:03 +0100

    added 15-Mongoose Folder

---
+++ class-notes/15-BlogApi_Mongoose/index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/*------------------------------------------
+
+EXPRESSJS -- BLOG PROJECT with Mongoose 
+
+-------------------------------------------*/
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+// Middlewares:
+app.use(express.json())
+require('express-async-errors')
+
+app.all('/', (req, res) => {
+    res.send('WELCOME TO BLOG API')
+})
+
+// continue from here...
+
+// Catch Errors:
+app.use(require('./src/middlewares/errorHandler'))
+
+/* ------------------------------------------------------- */
+
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
