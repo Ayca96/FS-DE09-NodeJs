3cbe69562a8fcd65fa61dff26248c77a25ba8bbe
Ayca96 <aycakubra1@gmail.com>
2024-11-11 09:46:26 +0100

    "Removed .env sample file and updated department.js controller and department.js model files with new code"

--- class-notes/19-PersonalApi/src/models/department.js
+++ class-notes/19-PersonalApi/src/models/department.js
@@ -4,3 +4,20 @@
 ------------------------------------------------------- */
 
 /* ------------------------------------------------------- */
+
+const {Schema, model, Collection} = require('mongoose');
+
+const DepartmentSchema = new Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true
+    }
+
+},{
+    collections:'departments',
+    timestamps:true,
+})
+
+module.exports = model("Department",DepartmentSchema)
