51a307b3e24b111b05a96c3a6ba0c8828a66907c
Ayca96 <aycakubra1@gmail.com>
2024-12-04 09:12:59 +0100

    "Updated dependencies, removed .env-sample files, and modified user controller, models, routes, and middlewares."

--- class-notes/28-Stock_API/_projectStarter/src/middlewares/permissions.js
+++ class-notes/28-Stock_API/_projectStarter/src/middlewares/permissions.js
@@ -4,13 +4,24 @@
 ------------------------------------------------------- */
 // Middleware: permissions
 
+function getUserInfo(req) {
+
+    let user = req.user
+    let isActive = req.user?.isActive
+    let isAdmin = req.user?.isAdmin
+    let isStaff = req.user?.isStaff
+  
+    return { user, isActive, isAdmin, isStaff }
+  }
+  
+
 module.exports = {
 
     isLogin: (req, res, next) => {
 
         // Set Passive:
         return next()
-
+        const { user, isActive } = getUserInfo(req)
         // any User:
         if (req.user && req.user.isActive) {
 
@@ -22,6 +33,18 @@ module.exports = {
             throw new Error('NoPermission: You must login.')
         }
     },
+
+    isStaffOrisAdmin: (req, res, next) => {
+
+        if (!(req.user?.isAdmin || req.user?.isStaff)) {
+          throw new Error("AuthorizationError: You must be an Admin or Staff to access this resource.", 403);
+        }
+    
+        if (!req.user?.isActive) {
+          throw new CustomError("Your account is not active. Please contact support.", 403);
+        }
+        next();
+      },
 
     isAdmin: (req, res, next) => {
 
@@ -36,7 +59,7 @@ module.exports = {
         } else {
 
             res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin.')
+            throw new customError('NoPermission: You must login and to be Admin.')
         }
     },
 
